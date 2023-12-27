f8bfd2a0436eee7eeca46ad06f99bafb40cb8ecb
Emirhan Tekin <73306905+emirhanttekin@users.noreply.github.com>
2023-12-27 09:56:50 +0300

    Add project files.

---
+++ WalksAPI/Data/TrWalksDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WalksAPI.Models.Domain;
+
+namespace WalksAPI.Data
+{
+    public class TrWalksDbContext : DbContext
+    {
+        public TrWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) 
+        {
+                        
+        }
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+    }
+}
