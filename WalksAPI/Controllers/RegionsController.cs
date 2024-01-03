2747c9796084dd4cc5c89ad0da7621616f9735bc
Emirhan Tekin <73306905+emirhanttekin@users.noreply.github.com>
2024-01-03 08:55:05 +0300

    Get Region By Id Action Method

---
+++ WalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WalksAPI.Data;
+using WalksAPI.Models.Domain;
+
+namespace WalksAPI.Controllers
+{
+   
+    // https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class RegionsController : ControllerBase
+    {
+        private readonly TrWalksDbContext dbContext;
+
+        public RegionsController(TrWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //Get All Regions
+        //Get : https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+          var regions =  dbContext.Regions.ToList();
+            
+
+            return Ok(regions);
+        }
+        //GET SINGLE REGION  (Get Region By Id)
+        //Get : https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute]Guid id) {
+           //var regions = dbContext.Regions.Find(id);
+           var region = dbContext.Regions.FirstOrDefault(r => r.Id == id);
+
+            if(region == null)
+            {
+                return NotFound();  
+            }
+
+            return Ok(region);
+
+        }
+    }
+}
--- WalksAPI/Data/TrWalksDbContext.cs
+++ WalksAPI/Data/TrWalksDbContext.cs
@@ -7,9 +7,12 @@ namespace WalksAPI.Data
     {
         public TrWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) 
         {
-                        
+            
         }
         public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
 
     }
 }
